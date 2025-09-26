b5520be04b6c9383616fbb4dd93e7a150fbb4ae6
Ryder <ryderjpm@gmail.com>
2025-09-26 00:15:02 -0400

    Fix __init__.py imports to match actual available functions

--- wikipedia_analysis/__init__.py
+++ wikipedia_analysis/__init__.py
@@ -10,19 +10,49 @@ __email__ = "ryderjpm@gmail.com"
 
 # Import main functionality for easy access
 from .database import create_constraints_and_indexes, batch_import_nodes, batch_import_relationships
-from .data_processing import clean_title, extract_wikipedia_links, parse_page
-from .queries import get_most_cited_articles, get_shortest_path
-from .analysis import calculate_pagerank, get_influential_articles
+from .data_processing import clean_title, parse_dump_file, batch_data
+from .queries import (
+    build_article_query,
+    build_category_query,
+    build_links_to_query,
+    build_belongs_to_query,
+    build_redirects_to_query,
+    build_pagerank_query,
+    build_shortest_path_query,
+    build_community_detection_query
+)
+from .analysis import (
+    calculate_pagerank,
+    find_shortest_path,
+    detect_communities,
+    calculate_centrality,
+    export_results,
+    measure_performance
+)
 
 __all__ = [
+    # Database functions
     'create_constraints_and_indexes',
     'batch_import_nodes', 
     'batch_import_relationships',
+    # Data processing functions
     'clean_title',
-    'extract_wikipedia_links',
-    'parse_page',
-    'get_most_cited_articles',
-    'get_shortest_path',
+    'parse_dump_file',
+    'batch_data',
+    # Query building functions
+    'build_article_query',
+    'build_category_query',
+    'build_links_to_query',
+    'build_belongs_to_query',
+    'build_redirects_to_query',
+    'build_pagerank_query',
+    'build_shortest_path_query',
+    'build_community_detection_query',
+    # Analysis functions
     'calculate_pagerank',
-    'get_influential_articles'
+    'find_shortest_path',
+    'detect_communities',
+    'calculate_centrality',
+    'export_results',
+    'measure_performance'
 ]
